f10cf24d4c640314b8b77039fb67d97b07e7359e
yadavkashish <kashishyadav9559@gmail.com>
2026-03-11 20:42:24 +0530

    scores

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './listCounters'   // add this
 
 const rootElement = document.getElementById('root')
 
